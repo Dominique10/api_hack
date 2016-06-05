8050eb17aebf8349d5e3eb132ef00754f3b1b0f8
cssdominique <dominique@christiansteven.com>
2016-06-05 09:55:51 -0400

    updated script file. Getting refuse to set header error

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+
+
+var code;
+
 $(document).ready( function(){
 	$(".myForm"). submit( function(e){
 		var location = $(this).find("input[name='userNameSearch']").val();
 		getData(location);
-		gotData(code);
-		console.log("{{treading}}");
+		getTwitterTrends(code);
+		//console.log("{{treading}}");
 		e.preventDefault();
+
+		$.post( "https://api.twitter.com/oauth2/token", 'grant_type=client_credentials')
+  .done(function( data ) {
+    alert( "Data Loaded: " + data );
+  });
 	})
 });
 
@@ -16,7 +25,30 @@ function getData(location){
 	var places = xml.getElementsByTagName("place");
 	for(var i = 0; i < places.length; i++) {
 	    var place1 = places[i];
-	    var code = place1.firstElementChild.textContent;
+	    code = place1.firstElementChild.textContent;
 	    return code;
 	}
 }
+
+ function getTwitterTrends(code){
+
+		$.ajaxSetup({
+			headers:{ 'POST':'/oauth2/token',
+				'Host': 'api.twitter.com',
+				'User-Agent': 'My Twitter App v1.0.23',
+				'Authorization': 'Basic WEk0NVhzeXJ5MkliWktSOHlCOU5FNWtmNzpyR1plS0ZBOFZkcEJUV2dKcGZjam1zM3NteXduSkFucm0xYTZsanB5UUZSSm9pZE9HdA==',
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+				'Content-Length': '29',
+				'Accept-Encoding': 'gzip'
+			}
+		});
+
+			$.ajax({
+			  type: "POST",
+			  url: 'https://api.twitter.com/1.1/trends/place.json?id='+code,
+			  'grant_type':'client_credentials',
+			  data: 'data',
+			  //success: success,
+			  dataType: 'jsonp'
+			});
+	}
